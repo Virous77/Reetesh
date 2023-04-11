f7f816a33df1e50fee726c737624c4daa8a832a3
Reetesh Kumar <101452588+Virous77@users.noreply.github.com>
2023-04-12 02:12:23 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as About } from "./About";
-export { default as Skills } from "./Skills";
-export { default as Contact } from "./Contact";
-export { default as Work } from "./Work";
