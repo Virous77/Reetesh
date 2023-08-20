3f57938726451fa4896327cf296d2716d6e8200b
Reetesh Kumar <rajreetesh7@gmail.com>
2023-08-20 23:03:57 +0530

    added latest address

--- updated/src/components/Contact/Contact.tsx
+++ updated/src/components/Contact/Contact.tsx
@@ -101,7 +101,7 @@ const Contact = () => {
               scrolling="no"
               marginHeight={0}
               marginWidth={0}
-              src="https://maps.google.com/maps?width=500&amp;height=400&amp;hl=en&amp;q=mahendru patna&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              src="https://maps.google.com/maps?width=500&amp;height=400&amp;hl=en&amp;q=Akash nidhi apartment Ahmedabad&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
             ></iframe>
           </div>
         </div>
