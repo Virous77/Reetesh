b90b5eeb53aa0598100e3af3d60bd63e49f93e4b
Reetesh Kumar <rajreetesh7@gmail.com>
2023-12-20 21:31:01 +0530

    update seo related changes

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -2,8 +2,15 @@ import { getAllBlogPosts } from "@/lib/post";
 
 export default async function sitemap() {
   const blogs = await getAllBlogPosts();
-
   const URL = "https://reetesh.vercel.app";
+
+  const restUrls = [
+    { url: `${URL}/`, lastModified: new Date() },
+    { url: `${URL}/blogs`, lastModified: new Date() },
+    { url: `${URL}/projects`, lastModified: new Date() },
+    { url: `${URL}/skills`, lastModified: new Date() },
+  ];
+
   const allBlogs = blogs.map(({ title, date }) => {
     const makeTile = (title: string) =>
       title.replace(/\s+/g, "-").toLowerCase();
@@ -13,5 +20,5 @@ export default async function sitemap() {
     };
   });
 
-  return [...allBlogs];
+  return [...restUrls, ...allBlogs];
 }
