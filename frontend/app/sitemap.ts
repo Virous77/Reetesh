b5e29f08d03eab97f487d5f7129fadb84ed68470
Reetesh Kumar <rajreetesh7@gmail.com>
2023-12-23 23:02:06 +0530

    update domain

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -2,7 +2,7 @@ import { getAllBlogPosts } from "@/lib/post";
 
 export default async function sitemap() {
   const blogs = await getAllBlogPosts();
-  const URL = "https://reetesh.vercel.app";
+  const URL = "https://reetesh.in";
 
   const restUrls = [
     {
