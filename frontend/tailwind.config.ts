c780ee482211c814a4a9681903d32e54383c47f1
Reetesh Kumar <rajreetesh7@gmail.com>
2024-01-19 10:13:39 +0530

    added theme changes

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -85,5 +85,12 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [],
+  },
 };
