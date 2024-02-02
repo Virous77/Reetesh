9984f1a5c92283ee9c574ca52fe7fa3c903f7791
Reetesh Kumar <rajreetesh7@gmail.com>
2024-02-02 23:35:45 +0530

    added navigation on blog page

--- frontend/app/blogs/page.tsx
+++ frontend/app/blogs/page.tsx
@@ -8,7 +8,7 @@ const BlogPage = async () => {
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
   return (
-    <main className=" md:grid flex l-template items-start gap-4 h-fit md:h-screen md:p-[30px] relative flex-col pb-4 md:pb-0">
+    <main className=" md:grid flex l-template items-start gap-4 h-fit md:h-screen md:p-[30px] md:pt-0 relative flex-col pb-4 md:pb-0">
       <Blog blogs={blogs} />
       <Sidebar />
     </main>
