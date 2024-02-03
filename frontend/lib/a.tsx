aadaa6e65b0d3cd22e348f00b0e21b5ec61208cc
Reetesh Kumar <rajreetesh7@gmail.com>
2024-02-03 11:50:53 +0530

    updated links action

---
+++ frontend/lib/a.tsx
@@ -0,0 +1,13 @@
+export const A = ({ children, ...props }: any) => {
+  return (
+    <a
+      {...props}
+      className="font-medium underline underline-offset-4"
+      target="_blank"
+      referrerPolicy="no-referrer"
+      title="Opens in a new tab"
+    >
+      {children}
+    </a>
+  );
+};
