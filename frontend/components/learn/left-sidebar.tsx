f6ff2ca38de95502305169365ec7e46fa6219191
Reetesh Kumar <rajreetesh7@gmail.com>
2024-03-23 23:20:29 +0530

    fixed css responsive issue

--- frontend/components/learn/left-sidebar.tsx
+++ frontend/components/learn/left-sidebar.tsx
@@ -11,26 +11,25 @@ type TLeftSidebar = {
 const LeftSidebar: React.FC<TLeftSidebar> = ({ id, course, topics }) => {
   return (
     <section className=" lg:border-r">
-      <div className="sticky top-0">
-        <div className="md:p-4">
-          <h2 className="mb-4 text-2xl font-bold text-default ">
-            {course.metadata}
-          </h2>
-          <ul className="mt-4 hidden overflow-y-scroll md:block lg:h-[80vh]">
-            {topics.map((topic) => (
-              <li key={topic.title} className="mb-2">
-                <Link
-                  href={`/learn/${id}/${slugify(topic.title)}`}
-                  className="text-default hover:text-primary"
-                >
-                  {topic.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div className="mb-4 block md:hidden">
-            <SelectComp topics={topics} id={id} />
-          </div>
+      <div className="md:p-4">
+        <h2 className="mb-4 text-2xl font-bold text-default ">
+          {course.metadata}
+        </h2>
+        <ul className="mt-4 hidden overflow-y-scroll lg:block lg:h-[80vh]">
+          {topics.map((topic) => (
+            <li key={topic.title} className="mb-2">
+              <Link
+                href={`/learn/${id}/${slugify(topic.title)}`}
+                className="text-default hover:text-primary"
+              >
+                {topic.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className="mb-4 block lg:hidden">
+          <SelectComp topics={topics} id={id} />
         </div>
       </div>
     </section>
