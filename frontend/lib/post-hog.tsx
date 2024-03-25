4ae76d0cf0f9a975877050b169a88f84d1cc1516
Reetesh Kumar <rajreetesh7@gmail.com>
2024-03-25 10:46:42 +0530

    fixed page view count issue

--- frontend/lib/post-hog.tsx
+++ frontend/lib/post-hog.tsx
@@ -6,7 +6,6 @@ import { PostHogProvider } from 'posthog-js/react';
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    capture_pageview: false,
     ui_host: 'https://app.posthog.com',
     debug: false,
   });
