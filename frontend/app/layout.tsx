14176e9991d2e33af96c4d107d3bd702f07d9a6d
Reetesh Kumar <rajreetesh7@gmail.com>
2024-03-28 22:44:53 +0530

    removed GTM

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -48,18 +48,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script id="gtm-script" strategy="beforeInteractive">
-          {`(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != "dataLayer" ? "&l=" + l : "";
-            j.async = true;
-            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-            f.parentNode.insertBefore(j, f);
-          })(window, document, "script", "dataLayer", "GTM-PPW7TBMX")`}
-        </Script>
+     
         <meta
           name="google-site-verification"
           content="ymnya5tVzcP4bwvG455V0AZC282mwI4Iy3mnS3uBTD0"
@@ -83,10 +72,6 @@ export default function RootLayout({
           href="/favicon-16x16.png"
         />
         <link rel="preconnect" href="https://res.cloudinary.com" />
-        <Script
-          strategy="beforeInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-XY0Y1HDGMT"
-        />
         <Script strategy="beforeInteractive" id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -99,14 +84,6 @@ export default function RootLayout({
       </head>
       <PHProvider>
         <body className={`${poppins.className} bg-white dark:bg-black`}>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-PPW7TBMX"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
           <ThemeProviderComp attribute="class" defaultTheme="dark">
             <Provider>
               <PostHogPageView />
