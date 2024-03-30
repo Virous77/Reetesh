e08d4b6ff3312bfb34dc9bab055e1aea6d390c86
Reetesh Kumar <rajreetesh7@gmail.com>
2024-03-30 11:27:04 +0530

    improved search on blogs page

--- frontend/components/blog/blog.tsx
+++ frontend/components/blog/blog.tsx
@@ -30,7 +30,7 @@ const Blog: React.FC<TBlog> = ({ blogs }) => {
         </div>
       </div>
 
-      <ul className=" grid grid-cols-1 items-start gap-4 pb-3 lg:grid-cols-2">
+      <ul className=" grid grid-cols-1 items-start gap-4 pb-3 md:grid-cols-2">
         {blogs.map((blog) => (
           <li key={blog._id}>
             <Card className=" w-full border-none bg-transparent hover:bg-popover">
--- frontend/components/common/command-search.tsx
+++ frontend/components/common/command-search.tsx
@@ -12,6 +12,8 @@ import {
 } from '@/components/ui/command';
 import { Post } from '@/.contentlayer/generated';
 import { useRouter } from 'next/navigation';
+import { usePostHog } from 'posthog-js/react';
+import { getLocalData, slugify } from '@/utils/utils';
 
 type TCommandSearch = {
   blogs: Post[];
@@ -19,12 +21,51 @@ type TCommandSearch = {
 
 const CommandSearch: React.FC<TCommandSearch> = ({ blogs }) => {
   const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState<string[] | []>([]);
   const router = useRouter();
+  const { capture } = usePostHog();
+
+  const handleEvent = (type: string, blog?: string) => {
+    if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
+      capture('searched_blog', {
+        type: type,
+        blog: blog || '',
+      });
+    }
+  };
+
+  const handleRecentSearch = (params: string) => {
+    const recentSearch: string[] = getLocalData('recentSearch');
+    if (recentSearch && recentSearch.includes(params)) {
+      const updatedRecentSearch = [
+        params,
+        ...recentSearch.filter((item: string) => item !== params),
+      ];
+      localStorage.setItem('recentSearch', JSON.stringify(updatedRecentSearch));
+      return;
+    }
+
+    if (recentSearch) {
+      if (recentSearch.length >= 5) {
+        recentSearch.pop();
+      }
+      recentSearch.unshift(params);
+      localStorage.setItem('recentSearch', JSON.stringify(recentSearch));
+    } else {
+      localStorage.setItem('recentSearch', JSON.stringify([params]));
+    }
+  };
 
   useEffect(() => {
+    const recentSearch = getLocalData('recentSearch');
+    if (recentSearch) {
+      setSearch(recentSearch);
+    }
+
     const down = (e: KeyboardEvent) => {
       if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
+        handleEvent('mouse_click');
         setOpen((open) => !open);
       }
     };
@@ -37,14 +78,17 @@ const CommandSearch: React.FC<TCommandSearch> = ({ blogs }) => {
     <React.Fragment>
       <div>
         <div
-          className=" flex cursor-pointer items-center gap-10 rounded border p-2 transition-colors duration-200 ease-in-out hover:bg-accent"
-          onClick={() => setOpen(true)}
+          className=" flex cursor-pointer items-center gap-10 rounded border p-2 pr-6 transition-colors duration-200 ease-in-out hover:bg-accent md:pr-2"
+          onClick={() => {
+            setOpen(true);
+            handleEvent('search_click');
+          }}
         >
           <span className=" flex items-center gap-2">
             <Search size={16} className=" text-muted-foreground" />
             <p className=" text-[14px] text-muted-foreground">Search Posts</p>
           </span>
-          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] text-sm font-medium text-muted-foreground  opacity-100">
+          <kbd className="pointer-events-none  hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] text-sm font-medium text-muted-foreground opacity-100  md:inline-flex">
             <span className="text-xs">⌘</span>K
           </kbd>
         </div>
@@ -53,12 +97,37 @@ const CommandSearch: React.FC<TCommandSearch> = ({ blogs }) => {
         <CommandInput placeholder="Search Posts..." />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup>
+          {search.length > 0 && (
+            <CommandGroup heading="Recent Searches">
+              {search.map((item) => (
+                <CommandItem
+                  key={item}
+                  className=" flex items-center justify-between"
+                  onSelect={() => {
+                    handleEvent('recent_search_click', item);
+                    handleRecentSearch(item);
+                    router.push(`/blog/${slugify(item)}`);
+                  }}
+                >
+                  <div className=" flex items-center gap-3">
+                    <Search />
+                    <span>
+                      {item}
+                      <span className="hidden">a</span>
+                    </span>
+                  </div>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          )}
+          <CommandGroup heading="All Blogs">
             {blogs.map((blog) => (
               <CommandItem
                 key={blog._id}
                 className=" flex items-center justify-between"
                 onSelect={() => {
+                  handleEvent('blog_click', blog.title);
+                  handleRecentSearch(blog.title);
                   router.push(`/blog/${blog.slugAsParams}`);
                 }}
               >
