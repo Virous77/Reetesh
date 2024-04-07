4ff46b26b3cafe724e51a23082d052cf768bb1e1
Reetesh Kumar <rajreetesh7@gmail.com>
2024-04-07 18:16:41 +0530

    removed posthog

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,14 +3,6 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/:path*',
-        destination: 'https://app.posthog.com/:path*',
-      },
-    ];
-  },
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
