c11695a3ee9c14ef2dfbb85a0fe089ed7d13e9c5
Reetesh Kumar <rajreetesh7@gmail.com>
2024-04-08 13:44:47 +0530

    added blog on react nested comment

--- cv/components/cv/print.tsx
+++ cv/components/cv/print.tsx
@@ -1,7 +1,17 @@
 "use client";
 import { useEffect } from "react";
+import { Button } from "../ui/button";
 
 const Print = () => {
+  const handleClickPrint = () => {
+    const credit = document.getElementById("credit");
+    if (credit) {
+      credit.style.display = "none";
+      window.print();
+      credit.style.display = "flex";
+    }
+  };
+
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
@@ -18,18 +28,27 @@ const Print = () => {
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
+
   return (
     <footer
       className=" flex items-center justify-center w-full mt-4 gap-4"
       id="credit"
     >
-      <div className=" flex items-center gap-3">
+      <div className="  items-center gap-3 md:flex hidden">
         <p className=" font-serif">Print</p>
         <kbd className="pointer-events-none  hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] text-sm font-medium text-muted-foreground opacity-100  md:inline-flex">
           <span className="text-xs">⌘</span>
           <span className=" text-[10px]">K</span>
         </kbd>
       </div>
+
+      <Button
+        variant="ghost"
+        className=" font-serif block  md:hidden"
+        onClick={handleClickPrint}
+      >
+        Download
+      </Button>
 
       <span className=" flex items-center   gap-1 text-xs">
         &copy;
