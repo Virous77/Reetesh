6171a519f6910f3dcd0c110050c94f50c7edc440
Reetesh Kumar <rajreetesh7@gmail.com>
2024-04-10 09:46:12 +0530

    removed code config and prefetch turn on

--- frontend/components/tab/navigation.tsx
+++ frontend/components/tab/navigation.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 const Navigation = () => {
   return (
     <div className=" mt-5 flex items-center gap-7">
-      <Link href="/blogs" aria-label="Visit all Blogs" prefetch={false}>
+      <Link href="/blogs" aria-label="Visit all Blogs">
         <span className="flex cursor-pointer items-center gap-2 font-mono text-[13px] text-default hover:text-defaultMax">
           <FileText size={17} /> Blogs
         </span>
       </Link>
 
-      <Link href="/skills" prefetch={false}>
+      <Link href="/skills">
         <span className=" flex cursor-pointer items-center gap-2 font-mono text-[13px] text-default hover:text-defaultMax">
           <Target size={17} /> Skills
         </span>
