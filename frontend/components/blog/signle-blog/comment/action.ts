59066d2767537bf6153aac46514e3eba0f177017
Reetesh Kumar <rajreetesh7@gmail.com>
2024-06-13 01:42:23 +0530

    added redis for comment spam issue and  removed unused code

--- frontend/components/blog/signle-blog/comment/action.ts
+++ frontend/components/blog/signle-blog/comment/action.ts
@@ -6,6 +6,7 @@ import dbConnect from '@/db/mongoose';
 import blogComments, { TBlog } from '@/models/blog-comments';
 import CommentEmail from '@/components/email/blogComment-mail';
 import { Resend } from 'resend';
+import { createClient } from '@vercel/kv';
 
 const schema = z.object({
   blogId: z.string().min(1, { message: 'Blog id is required' }),
@@ -35,11 +36,43 @@ const commentMail = async (comment: string, blogId: string) => {
   }
 };
 
+const restrictSpamComment = async (userId: string) => {
+  const kv = createClient({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+  });
+
+  const user: number | null = await kv.get(`comment:${userId}`);
+
+  if (user === 5) {
+    throw new Error('You have reached the maximum comment limit');
+  } else if (user) {
+    await kv.set(`comment:${userId}`, user + 1, {
+      ex: 300,
+    });
+  } else {
+    await kv.set(`comment:${userId}`, 1, {
+      ex: 300,
+    });
+  }
+};
+
+export const commonError = (error: unknown, message: string) => {
+  if (error instanceof z.ZodError) {
+    throw new Error(error.errors[0].message);
+  } else if (error instanceof Error) {
+    throw new Error(error.message);
+  }
+
+  throw new Error(message);
+};
+
 type TInput = z.infer<typeof schema>;
 
 export const createComment = async (params: TInput) => {
   try {
     const validate = schema.parse(params);
+    await restrictSpamComment(validate.userId);
 
     await dbConnect();
     await blogComments.create({
@@ -53,16 +86,15 @@ export const createComment = async (params: TInput) => {
 
     return 'Comment added successfully';
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      throw new Error(error.errors[0].message);
-    }
-    throw new Error('Failed to add comment');
+    commonError(error, 'Failed to add comment');
   }
 };
 
 export const createReplyComment = async (params: TInput) => {
   try {
     const validate = schema.parse(params);
+    await restrictSpamComment(validate.userId);
+
     await dbConnect();
     const newComment: TBlog = await blogComments.create({
       ...validate,
@@ -76,9 +108,6 @@ export const createReplyComment = async (params: TInput) => {
       commentMail(validate.comment, validate.blogId);
     }
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      throw new Error(error.errors[0].message);
-    }
-    throw new Error('Failed to add comment');
+    commonError(error, 'Failed to add comment');
   }
 };
