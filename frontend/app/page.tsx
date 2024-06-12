331c9dc3b1dc33975ba419be829e4caf6ccf9344
Reetesh Kumar <rajreetesh7@gmail.com>
2024-06-12 12:27:22 +0530

    added new blog on stripe payment links

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <main className="relative m-auto h-screen grid-cols-2 items-start gap-5 px-4 sm:max-w-full md:grid md:max-w-[1050px] md:overflow-hidden">
       <Author />
-      <section className="body flex flex-col  md:h-full md:gap-[60px] md:pb-8 md:pt-[70px]">
+      <section className="body flex flex-col md:h-full md:gap-[60px] md:pb-8 md:pt-[70px]">
         <About />
         <Recent />
         <Footer />
