ee1354d0b52806f163f333deee5883d775e9db81
Reetesh Kumar <rajreetesh7@gmail.com>
2024-07-02 19:41:55 +0530

    fixes modal padding for desktop view

--- frontend/components/ui/command.tsx
+++ frontend/components/ui/command.tsx
@@ -6,7 +6,7 @@ import { Command as CommandPrimitive } from 'cmdk';
 import { Search } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
-import { Credenza, CredenzaContent } from './responsive-modal';
+import { Credenza, CredenzaContent, CredenzaTitle } from './responsive-modal';
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -33,6 +33,8 @@ const CommandDialog = ({
   return (
     <Credenza {...props}>
       <CredenzaContent className="overflow-hidden rounded p-0 shadow-lg md:h-[500px] md:max-w-[700px]">
+        <CredenzaTitle className="hidden">Search Posts</CredenzaTitle>
+
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
