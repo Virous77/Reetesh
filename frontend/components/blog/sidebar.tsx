0dd874837b9da6fd3effa94ca40b3b440a07f822
Reetesh Kumar <rajreetesh7@gmail.com>
2024-07-09 02:09:29 +0530

    fixes styling

--- frontend/components/blog/sidebar.tsx
+++ frontend/components/blog/sidebar.tsx
@@ -29,9 +29,9 @@ const Sidebar = () => {
       />
       <CommonTab links={Links} type={false} />
 
-      <GoogleAdUnit className="mt-4 px-4 md:mt-[80px] md:px-0">
+      <GoogleAdUnit className="mt-4 !bg-background px-4 md:mt-[80px] md:px-0">
         <ins
-          className="adsbygoogle"
+          className="adsbygoogle max-w-[250px] md:max-w-full"
           style={{
             display: 'block',
           }}
