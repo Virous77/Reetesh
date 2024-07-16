c5e8bcc0a89f8f92c1e3201c46df41687728d773
Reetesh Kumar <rajreetesh7@gmail.com>
2024-07-17 02:47:38 +0530

    added link preview for blog

---
+++ frontend/app/api/preview/route.ts
@@ -0,0 +1,73 @@
+import ogs from 'open-graph-scraper';
+import { createClient } from '@vercel/kv';
+import { NextRequest } from 'next/server';
+
+type TData = {
+  title: string | null;
+  description: string | null;
+  image: string | null;
+  key: string;
+};
+
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const url = searchParams.get('url');
+
+  if (!url) {
+    return new Response('url is required', {
+      status: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const kv = createClient({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+  });
+
+  const isCached: TData | null = await kv.get(`preview:${url}`);
+  if (isCached)
+    return new Response(JSON.stringify(isCached), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+  const options = { url };
+  const data = await ogs(options);
+  const { result } = data;
+
+  const href = new URL(url);
+  const image = result.ogImage
+    ? result.ogImage[0].url
+    : `${href.origin}${result.favicon}` || null;
+  const dataObj = {
+    title:
+      result.ogTitle ||
+      result.ogSiteName ||
+      result.twitterTitle ||
+      result.dcTitle ||
+      null,
+    description:
+      result.ogDescription ||
+      result.twitterDescription ||
+      result.dcDescription ||
+      null,
+    image: !image?.includes('http') ? `${href.origin}${image}` : image,
+    key: url,
+  };
+
+  //expiry for 2 days
+  await kv.set(`preview:${url}`, JSON.stringify(dataObj), {
+    ex: 345600,
+  });
+  return new Response(JSON.stringify(dataObj), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
--- frontend/lib/a.tsx
+++ frontend/lib/a.tsx
@@ -1,11 +1,179 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+
+import { ToolTipLink } from '@/components/ui/tooltip';
+import React, { useState } from 'react';
+import { useMediaQuery } from './media-query';
+
+type TDetails = {
+  title: string | null;
+  description: string | null;
+  image: string | null;
+  key: string;
+};
+
+const desktop = '(min-width: 768px)';
+
 export const A = ({ children, ...props }: any) => {
+  const isDesktop = useMediaQuery(desktop);
+  const [open, setOpen] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [details, setDetails] = useState<TDetails[] | []>([]);
+
+  const handleHover = async (href: string) => {
+    setOpen(href);
+    if (href.includes('mailto:')) return;
+    if (href[0] === '#') return;
+
+    try {
+      const isCached =
+        details.length > 0 && details.find((item) => item.key === href);
+      if (isCached) return;
+      setIsLoading(true);
+      const res = await fetch('/api/preview?url=' + href, {
+        cache: 'no-cache',
+      });
+      const data = await res.json();
+      const { title, description, image } = data;
+      setDetails((prev) => [...prev, { title, description, image, key: href }]);
+      setIsLoading(false);
+    } catch (error) {
+      setDetails((prev) => [
+        ...prev,
+        { title: 'error', description: null, image: null, key: href },
+      ]);
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
+  const currentLink = details.find((item) => item.key === open);
+  const isShow = props.href[0] === '#';
+
+  return (
+    <React.Fragment>
+      {isDesktop ? (
+        <ToolTipLink
+          name={
+            <Content
+              currentLink={currentLink}
+              isLoading={isLoading}
+              open={open}
+            />
+          }
+          duration={isShow ? 10000000 : 100}
+        >
+          <a
+            {...props}
+            className="font-medium underline underline-offset-4"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            onMouseEnter={(e) => {
+              e.preventDefault();
+              handleHover(props.href);
+            }}
+          >
+            {children}
+          </a>
+        </ToolTipLink>
+      ) : (
+        <MobileA {...props}>{children}</MobileA>
+      )}
+    </React.Fragment>
+  );
+};
+
+const Content = ({
+  currentLink,
+  isLoading,
+  open,
+}: {
+  currentLink: TDetails | undefined;
+  isLoading: boolean;
+  open: string;
+}) => {
+  return (
+    <div className="h-full w-full">
+      {isLoading ? (
+        <p className="m-0 w-full p-0 text-center text-foreground">Loading...</p>
+      ) : (
+        <React.Fragment>
+          {currentLink?.title === 'error' ? (
+            <div>
+              <p
+                className="m-0 w-full p-0 text-center"
+                style={{
+                  fontSize: '0.75rem',
+                  color: 'red',
+                }}
+              >
+                opps! no content found
+              </p>
+              <VisitLink href={open || '#'} />
+            </div>
+          ) : (
+            <div>
+              <h3 className="mb-1 mt-0 text-base font-semibold">
+                {currentLink?.title}
+              </h3>
+              <p
+                className="text-default"
+                style={{
+                  fontSize: '0.75rem',
+                }}
+              >
+                {currentLink?.description}
+              </p>
+              {currentLink?.image && (
+                <img
+                  src={currentLink.image}
+                  alt={currentLink.title || ''}
+                  title={currentLink.title || ''}
+                  aria-label={currentLink.title || ''}
+                  onError={(e) => {
+                    e.currentTarget.src = '/favicon.ico';
+                  }}
+                  className="mt-3 h-32 w-full object-cover shadow"
+                  style={{
+                    borderRadius: '0.5rem',
+                    height: '8rem',
+                  }}
+                />
+              )}
+              {currentLink && <VisitLink href={currentLink?.key || '#'} />}
+            </div>
+          )}
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
+
+const MobileA = ({ children, ...props }: any) => {
   return (
     <a
       {...props}
       className="font-medium underline underline-offset-4"
       title="content link"
+      aria-label="visit link"
     >
       {children}
+    </a>
+  );
+};
+
+const VisitLink = ({ href }: { href: string }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      className="mt-2 flex items-center justify-center font-medium text-foreground underline underline-offset-4"
+      title="content link"
+      aria-label="visit link"
+    >
+      Visit to learn more
     </a>
   );
 };
