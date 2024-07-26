0ff3918bf0ec364fc0bed18dbd8f9dd956a40357
Reetesh Kumar <rajreetesh7@gmail.com>
2024-07-26 23:39:43 +0530

    fixes required missing props issue

--- frontend/components/blog/signle-blog/comment/preview.tsx
+++ frontend/components/blog/signle-blog/comment/preview.tsx
@@ -12,7 +12,7 @@ const Preview: React.FC<TPreview> = ({ comment, isComment }) => {
       className={`just-way prose-headings:font-cal prose prose-base prose-neutral ${isComment ? 'mb-[18px]' : 'mb-[6px]'} min-h-[98px] rounded-lg border p-2 dark:prose-invert prose-a:text-default prose-a:underline prose-a:underline-offset-4 hover:prose-a:text-defaultMax prose-blockquote:font-light prose-img:rounded-lg`}
     >
       {comment ? (
-        <CommentMarkdown comment={comment} />
+        <CommentMarkdown comment={comment} isCode={comment.includes('```')} />
       ) : (
         <p className="text-foreground-secondary px-1 pt-[13px]">
           Nothing to preview
