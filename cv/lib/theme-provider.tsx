396dfcc50129fc58ec1ae37d52f933fb3846cd55
Reetesh Kumar <rajreetesh7@gmail.com>
2024-12-14 08:43:07 +0530

    updated cv app to next15 and react19

--- cv/lib/theme-provider.tsx
+++ cv/lib/theme-provider.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
 
 const ThemeProviderComp = ({ children, ...props }: ThemeProviderProps) => {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
