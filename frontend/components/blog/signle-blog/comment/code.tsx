168ff3a199792d082a77f32b81b35eacc482c7a3
Reetesh Kumar <rajreetesh7@gmail.com>
2025-01-20 12:12:03 +0530

    fixes build error and updated to latest version

--- frontend/components/blog/signle-blog/comment/code.tsx
+++ frontend/components/blog/signle-blog/comment/code.tsx
@@ -1,7 +1,9 @@
 import CommentCopy from '@/lib/comment-copy';
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismLight } from 'react-syntax-highlighter';
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const SyntaxHighlighter = PrismLight as any;
 
 SyntaxHighlighter.registerLanguage('jsx', jsx);
 
